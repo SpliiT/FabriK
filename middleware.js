40769c42705fe599e89d30daa336886682821fef
SpliiT <spliitdev@gmail.com>
2025-04-10 20:15:06 +0200

    mb

--- middleware.js
+++ middleware.js
@@ -1,7 +1,10 @@
-import { authMiddleware } from "@clerk/nextjs"; 
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default authMiddleware();
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ["/((?!_next|api|favicon.ico).*)"], 
-};
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
