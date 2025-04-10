c2e37e6758113500cfc4cf6be729c4ca4b8baa8a
SpliiT <spliitdev@gmail.com>
2025-04-10 20:10:31 +0200

    middle ware

--- middleware.js
+++ middleware.js
@@ -1,12 +1,8 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: ["/api/webhook", "/api/(.*)"]
-});
+export default authMiddleware();
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/']
+  matcher: ["/((?!_next|api|favicon.ico).*)"],
+  runtime: 'nodejs',
 };
